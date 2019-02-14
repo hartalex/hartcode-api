1be1200ececff827f23e0dbd36155605f653f89e
Alex Hart <hartalex@gmail.com>
2019-02-13 21:09:53 -0600

    feat(server.js): Add CORS headers to each request

--- src/server/server.js
+++ src/server/server.js
@@ -2,7 +2,11 @@ import express from 'express'
 import routes from './routes/routes'
 const port = process.env.PORT || 8811
 const app = express()
-
+app.use(function (req, res, next) {
+  res.set('Access-Control-Allow-Origin', '*')
+  res.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
 routes(app)
 
 app.listen(port)
